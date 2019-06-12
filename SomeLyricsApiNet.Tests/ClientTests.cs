9bbbbaef164f752ba5776e5733d76703ad0f996b
Joel Parkinson <joelparkinson@live.co.uk>
2019-06-12 20:10:01 +0100

    Add more Client#GetBaseLyrics tests
    - Add Client_GetBaseLyrics_UrlShouldNotBeNull
    - Add Client_GetBaseLyrics_ImageUrlShouldNotBeNull
    - Add Client_GetBaseLyrics_TitleShouldNotBeNull
    - Add Client_GetBaseLyrics_AuthorShouldNotBeNull
    - Add Client_GetBaseLyrics_LyricsShouldReturnString

--- SomeLyricsApiNet.Tests/ClientTests.cs
+++ SomeLyricsApiNet.Tests/ClientTests.cs
@@ -39,5 +39,56 @@ namespace SomeLyricsApiNet.Tests
             var result = await client.GetBaseLyrics(search);
             Assert.NotNull(result.Lyrics);
         }
+
+        [Theory]
+        [InlineData("Feel it still")]
+        [InlineData("We will rock you")]
+        public async Task Client_GetBaseLyrics_UrlShouldNotBeNull(string search)
+        {
+            var client = new SomeLyricsClient();
+            var result = await client.GetBaseLyrics(search);
+            Assert.NotNull(result.Links.GeniusUrl);
+        }
+
+        [Theory]
+        [InlineData("Feel it still")]
+        [InlineData("We will rock you")]
+        public async Task Client_GetBaseLyrics_ImageUrlShouldNotBeNull(string search)
+        {
+            var client = new SomeLyricsClient();
+            var result = await client.GetBaseLyrics(search);
+            Assert.NotNull(result.Thumbnail.Url);
+        }
+
+        [Theory]
+        [InlineData("Feel it still")]
+        [InlineData("We will rock you")]
+        public async Task Client_GetBaseLyrics_TitleShouldNotBeNull(string search)
+        {
+            var client = new SomeLyricsClient();
+            var result = await client.GetBaseLyrics(search);
+            Assert.NotNull(result.Title);
+        }
+
+        [Theory]
+        [InlineData("Feel it still")]
+        [InlineData("We will rock you")]
+        public async Task Client_GetBaseLyrics_AuthorShouldNotBeNull(string search)
+        {
+            var client = new SomeLyricsClient();
+            var result = await client.GetBaseLyrics(search);
+            Assert.NotNull(result.Author);
+        }
+
+        [Fact]
+        public async Task Client_GetBaseLyrics_LyricsShouldReturnString()
+        {
+            var search = "We will rock you";
+            var expected = "[Verse 1]\nBuddy, you're a boy, make a big noise\nPlaying in the street, gonna be a big man someday\nYou got mud on your face, you big disgrace\nKicking your can all over the place, singing\n\n[Chorus]\nWe will, we will rock you\nWe will, we will rock you\n\n[Verse 2]\nBuddy, you're a young man, hard man\nShouting in the street, gonna take on the world someday\nYou got blood on your face, you big disgrace\nWaving your banner all over the place\n\n[Chorus]\nWe will, we will rock you\nSing it out\nWe will, we will rock you\n\n[Verse 3]\nBuddy, you're an old man, poor man\nPleading with your eyes, gonna make you some peace someday\nYou got mud on your face, big disgrace\nSomebody better put you back into your place\n\n[Chorus]\nWe will, we will rock you, sing it\nWe will, we will rock you, everybody\nWe will, we will rock you, hmm\nWe will, we will rock you, alright\n\n[Guitar Solo]";
+            var client = new SomeLyricsClient();
+            var result = await client.GetBaseLyrics(search);
+
+            Assert.Equal(expected, result.Lyrics);
+        }
     }
 }
