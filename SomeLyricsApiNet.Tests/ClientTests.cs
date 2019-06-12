427f700699dce4403870091dbff72fc9e8b41e5a
Joel Parkinson <joelparkinson@live.co.uk>
2019-06-12 20:00:25 +0100

    Implement more test (see desc)
    - Add Client_GetBaseLyricsAsync_ShouldReturnLyricModelOnSuccess
    - Add Client_GetBaseLyrics_LyricsShouldNotBeNull

--- SomeLyricsApiNet.Tests/ClientTests.cs
+++ SomeLyricsApiNet.Tests/ClientTests.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Threading.Tasks;
+using SomeLyricsApiNet.Models;
 using Xunit;
 
 namespace SomeLyricsApiNet.Tests
@@ -11,11 +13,31 @@ namespace SomeLyricsApiNet.Tests
         [InlineData(null)]
         [InlineData("blah blah blah")]
         [InlineData("Can't possibly be a song!")]
-        public void Client_GetBaseLyricsAsync_ThrowExceptionOnBadSearch(string url)
+        public void Client_GetBaseLyricsAsync_ThrowExceptionOnBadSearch(string search)
         {
             var client = new SomeLyricsClient();
-            var ex = Record.ExceptionAsync(async () => await client.GetBaseLyrics(url));
+            var ex = Record.ExceptionAsync(async () => await client.GetBaseLyrics(search));
             Assert.NotNull(ex);
+        }
+
+        [Theory]
+        [InlineData("Feel it still")]
+        [InlineData("We will rock you")]
+        public async Task Client_GetBaseLyricsAsync_ShouldReturnLyricModelOnSuccess(string search)
+        {
+            var client = new SomeLyricsClient();
+            var result = await client.GetBaseLyrics(search);
+            Assert.IsType<Lyric>(result);
+        }
+
+        [Theory]
+        [InlineData("Feel it still")]
+        [InlineData("We will rock you")]
+        public async Task Client_GetBaseLyrics_LyricsShouldNotBeNull(string search)
+        {
+            var client = new SomeLyricsClient();
+            var result = await client.GetBaseLyrics(search);
+            Assert.NotNull(result.Lyrics);
         }
     }
 }
