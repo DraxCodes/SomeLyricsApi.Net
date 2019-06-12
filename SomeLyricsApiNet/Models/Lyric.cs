521ac55d569a0fd5f0e9bb8926a4d393ab1acb94
Joel Parkinson <joelparkinson@live.co.uk>
2019-06-12 19:50:55 +0100

    Implement Lyrics Model

---
+++ SomeLyricsApiNet/Models/Lyric.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+
+namespace SomeLyricsApiNet.Models
+{
+    public class Lyric
+    {
+        [JsonProperty("title")] public string Title { get; set; }
+        [JsonProperty("author")] public string Author { get; set; }
+        [JsonProperty("lyrics")] public string Lyrics { get; set; }
+        [JsonProperty("thumbnail")] public Thumbnail Thumbnail { get; set; }
+        [JsonProperty("links")] public Links Links { get; set; }
+    }
+
+    public class Thumbnail
+    {
+        [JsonProperty("genius")] public string Url { get; set; }
+    }
+
+    public class Links
+    {
+        [JsonProperty("genius")] public string GeniusUrl { get; set; }
+    }
+
+}
