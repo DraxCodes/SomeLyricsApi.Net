66db9f9dd732693a88e97f450c26a4ac14564cfa
Joel Parkinson <joelparkinson@live.co.uk>
2019-06-12 19:51:17 +0100

    Implement Di service extension

---
+++ SomeLyricsApiNet/Extensions/ServiceExtension.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SomeLyricsApiNet.Abstractions;
+
+namespace SomeLyricsApiNet.Extensions
+{
+    public static class ServiceExtension
+    {
+        public static IServiceCollection UseSomeLyricsApi(this IServiceCollection services)
+        {
+            services.AddSingleton<ISomeLyricsClient, SomeLyricsClient>();
+            return services;
+        }
+    }
+}
